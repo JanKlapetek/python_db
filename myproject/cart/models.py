1a8baafd671bdae380e7158aa5d7dc711aec7ba3
Jan_win <jan.klapetek@gmail.com>
2024-09-12 18:47:16 +0200

    uprava models

--- myproject/cart/models.py
+++ myproject/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CartItem(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
